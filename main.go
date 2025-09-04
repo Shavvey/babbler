495f3ce57a71a3ef9c837b35cb6bf322331b9db9
Cole Johnson <coledjohn@gmail.com>
2025-09-04 14:53:23 -0600

    Renaming last time for realsies

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	babbler "github.com/Shavvey/babbler"
+	babbler "github.com/Shavvey/babbler/src"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	babbler.GetText("whatever.txt")
 }
